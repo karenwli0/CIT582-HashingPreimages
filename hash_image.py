19a42354319bd0bf27b389effaadfd6bf5a42660
unknown <karenwli@seas.upenn.edu>
2022-01-18 10:31:47 -0800

    'third

--- hash_image.py
+++ hash_image.py
@@ -8,7 +8,8 @@ def hash_preimage(target_string):
     if not all([x in '01' for x in target_string]):
         print("Input should be a string of bits")
         return
-    nonce = b'\x00'
+    x = b'\x00'
+    # target_bit = target_string.encode('utf-8')
     k = len(target_string)
     x = random.choice(string.ascii_letters)
     x_sha = hashlib.sha256(x.encode('utf-8'))
@@ -17,6 +18,6 @@ def hash_preimage(target_string):
     while final_k_digits_x != target_string:
         x += random.choice(string.ascii_letters)
         x_sha = hashlib.sha256(x.encode('utf-8'))
-        final_k_digits_y = bin(int(x_sha.hexdigest(), base=16))[-k:]
+        final_k_digits_x = bin(int(x_sha.hexdigest(), base=16))[-k:]
 
-    return nonce
+    return x
---
+++ main.py
@@ -0,0 +1,4 @@
+from hash_image import hash_preimage
+
+a = '0000111'
+print(hash_preimage(a))
